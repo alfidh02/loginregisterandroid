866a5001ad090dcaa63fcc718806d51eb230e306
alfidh02 <alfidarmawan79@gmail.com>
2019-12-07 22:30:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidregisterandlogin/HomeActivity.java
@@ -0,0 +1,230 @@
+package com.example.androidregisterandlogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView name,email;
+    private Button btn_logout,btn_photo_upload;
+    SessionManager sessionManager;
+    String getId;
+    private static String URL_READ = "http://192.168.1.16/android_register_login/read_detail.php";
+    private static String URL_EDIT = "http://192.168.1.16/android_register_login/edit_detail.php";
+    private static final String TAG = HomeActivity.class.getSimpleName(); //getting the info
+    private Menu action;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        sessionManager = new SessionManager(this);
+        sessionManager.checkLogin();
+
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        btn_logout = findViewById(R.id.btn_logout);
+
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        getId = user.get(sessionManager.ID);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sessionManager.loguout();
+            }
+        });
+    }
+
+//getUserDetail
+    private void getUserDetail(){
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_READ,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        Log.i(TAG,response.toString());
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("read");
+
+                            if (success.equals("1")) {
+                                for (int i=0;i<jsonArray.length();i++){
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String strName = object.getString("name").trim();
+                                    String strEmail = object.getString("email").trim();
+
+                                    name.setText(strName);
+                                    email.setText(strEmail);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressDialog.dismiss();
+                            Toast.makeText(HomeActivity.this, "Error reading detail "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "Error reading detail "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id",getId);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getUserDetail();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_action,menu);
+
+        action = menu;
+        action.findItem(R.id.menu_save).setVisible(false);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_edit:
+                name.setFocusableInTouchMode(true);
+                email.setFocusableInTouchMode(true);
+
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(name, InputMethodManager.SHOW_IMPLICIT);
+
+                action.findItem(R.id.menu_edit).setVisible(false);
+                action.findItem(R.id.menu_save).setVisible(true);
+
+                return true;
+
+            case R.id.menu_save:
+
+                SaveEditDetail();
+
+                action.findItem(R.id.menu_edit).setVisible(true);
+                action.findItem(R.id.menu_save).setVisible(false);
+
+                name.setFocusableInTouchMode(false);
+                email.setFocusableInTouchMode(false);
+                name.setFocusable(false);
+                email.setFocusable(false);
+
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+//    save
+    private void SaveEditDetail() {
+
+        final String name = this.name.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String id = getId;
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Saving...");
+        progressDialog.show();
+
+        StringRequest  stringRequest = new StringRequest(Request.Method.POST, URL_EDIT,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Toast.makeText(HomeActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                                sessionManager.createSession(name,email,id);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            progressDialog.dismiss();
+                            Toast.makeText(HomeActivity.this, "Error "+e.toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "Error "+error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("id",id);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+}
